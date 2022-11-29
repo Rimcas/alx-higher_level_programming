37f14d2ca2c3db5f13a4820351ccd20642413bdb
Rimcas <eguducasidi0@gmail.com>
2022-11-29 05:57:43 -0800

    file checked

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include "lists.h"
 /**
- * check_cylce - This function checks if there is cycle in 
+ * check_cycle - This fucntion checks if there is cycle in
  * singly linked list
- * @list: pointer to the head
+ * @list: Pointer to the head
  *
  * Return: 0 if there is no cycle and 1 if otherwise
  */
@@ -13,21 +13,18 @@ int check_cycle(listint_t *list)
 	listint_t *check;
 	listint_t *follow;
 
-
 	if (!list)
-		return(0);
+		return (0);
 
 	check = list;
 	follow = list;
 	while (check && follow && check->next)
 	{
-		follow = follow->next->next;
+		follow = follow->next;
 		check = check->next->next;
 		if (check == follow)
 			return (1);
-	
 	}
 	return (0);
-
 
 }
