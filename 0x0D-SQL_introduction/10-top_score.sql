9319dcbae689fa1ecb950a05c22ad2dc42970219
rimca <eguducasidi0@gmail.com>
2023-02-15 10:32:52 +0000

    0x0D-SQL_introduction task

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,2 @@
+-- display and order record by score
+Select score, name From second_table ORDER BY score DESC;
---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,3 @@
+-- List record where score >= 10
+SELECT score,name From second_table Where score >= 10 order by score DESC;
+
